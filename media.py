53956a1f32e3e33ddc13235a6a6bd98bcf04c4bf
SwapnaliThakar <swapnalit@cybersapien.ca>
2017-08-15 10:06:20 -0400

    Fixed commenting and line length issues.

--- media.py
+++ media.py
@@ -4,7 +4,7 @@ import webbrowser
 class Movie():
     """This class initializes the details of a
     movie and shows trailer as required
-    
+
         movie_title (str): is the title of the movie
         movie_storyline (str): is the storyline of the movie
         poster_image (url): is the url from which to get the poster image
@@ -13,11 +13,15 @@ class Movie():
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
-        self.title = movie_title  # initializes movie title
-        self.storyline = movie_storyline  # initializes storyline of the movie
-        self.poster_image_url = poster_image  # initializes poster image
-        self.trailer_youtube_url = trailer_youtube  # initializes movie trailer url
-        
+        # initializes movie title
+        self.title = movie_title
+        # initializes storyline of the movie
+        self.storyline = movie_storyline
+        # initializes poster image
+        self.poster_image_url = poster_image
+        # initializes movie trailer url
+        self.trailer_youtube_url = trailer_youtube
+
     # Shows trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
